fc26c6324cda65b92e8cd1a9c19adfdfb296a73c
solllve <caseyhald@Caseys-MacBook-Pro.local>
2021-10-02 17:46:07 -0700

    init

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 3000;
-app.listen(port, () => {
-    console.log('test')
-});
-
 app.get('/', (req, res) => {
     res.send("welcome")
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log('test')
 });
